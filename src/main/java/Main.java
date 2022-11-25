7310fc074f5ad04c06d21a9d01e7fcbd5507c1a1
Christian Weck <weck.chr@gmail.com>
2022-11-25 11:35:26 +0100

    Tests written; Function written (nested for loops)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,51 @@
 public class Main {
 
     public static boolean checkPassword(String password) {
-        return true;
+        boolean lengthCheck = false;
+        boolean containsCapitals = false;
+        boolean containsNumbers = false;
+        boolean containsSpecials = false;
+
+        //char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char[] capitals = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        char[] numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        char[] specials = {'!', '.', ':', '-', '_', ',', ';', '+', '*'};
+        if(password.length() > 6) {
+            lengthCheck = true;
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+
+            for (char capital : capitals) {
+                if (password.charAt(i) == capital) {
+                    containsCapitals = true;
+                    break;
+                }
+            }
+            /*              -- OLD --
+            for (int j = 0; j < capitals.length; j++) {
+                if (password.charAt(i) == capitals[j]) {
+                    containsCapitals = true;
+                    break;
+                }
+            }
+
+             */
+            for (char number : numbers) {
+                if (password.charAt(i) == number) {
+                    containsNumbers = true;
+                    break;
+                }
+            }
+            for (char special : specials) {
+                if (password.charAt(i) == special) {
+                    containsSpecials = true;
+                    break;
+                }
+            }
+        }
+
+        return lengthCheck && containsCapitals && containsNumbers && containsSpecials;
     }
 
     public static void main(String[] args) {
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,73 +8,90 @@ class MainTest {
     @Test
     void checkPasswordTooShort() {
         //Given
-
+        String password = "2.Klm";
         //When
-
+        boolean actual = Main.checkPassword(password);
         //Then
+        //Assertions.assertEquals(false, actual);
+        Assertions.assertFalse(actual);
     }
 
     @Test
     void checkPasswordOnlyLetters() {
         //Given
-
+        String password = "gHilamsjesfa";
         //When
-
+        boolean actual = Main.checkPassword(password);
         //Then
+        //Assertions.assertEquals(false, actual);
+        Assertions.assertFalse(actual);
     }
 
     @Test
     void checkPasswordOnlyNumbers() {
         //Given
-
+        String password = "1947234827";
         //When
-
+        boolean actual = Main.checkPassword(password);
         //Then
+        //Assertions.assertEquals(false, actual);
+        Assertions.assertFalse(actual);
     }
 
     @Test
     void checkPasswordOnlySpecialCharacters() {
         //Given
-
+        String password = "-.-.-+-.+.-+.";
         //When
-
+        boolean actual = Main.checkPassword(password);
         //Then
+        //Assertions.assertEquals(false, actual);
+        Assertions.assertFalse(actual);
     }
 
     @Test
     void checkPasswordOnlyCapitals() {
         //Given
-
+        String password = "HDKSOFKCLEJF";
         //When
-
+        boolean actual = Main.checkPassword(password);
         //Then
+        //Assertions.assertEquals(false, actual);
+        Assertions.assertFalse(actual);
     }
 
     @Test
     void checkPasswordNoNumbers() {
         //Given
-
+        String password = "Ksm.opjsk--k";
         //When
-
+        boolean actual = Main.checkPassword(password);
         //Then
+        //Assertions.assertEquals(false, actual);
+        Assertions.assertFalse(actual);
     }
 
     @Test
     void checkPasswordNoSpecialCharacters() {
         //Given
-
+        String password = "kcHU9382msl";
         //When
-
+        boolean actual = Main.checkPassword(password);
         //Then
+        //Assertions.assertEquals(false, actual);
+        Assertions.assertFalse(actual);
     }
 
     @Test
     void checkPasswordNoCapitals() {
         //Given
-
+        String password = "hdk98nk.-d";
         //When
-
+        boolean actual = Main.checkPassword(password);
         //Then
+        //Assertions.assertEquals(false, actual);
+        Assertions.assertFalse(actual);
+
     }
 
 
