8b9eaadc6bd6fe45b1f1359e156c40cde685bf6e
Christian Weck <weck.chr@gmail.com>
2022-11-25 10:24:19 +0100

    First Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+
+    public static boolean checkPassword(String password) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void checkPasswordTooShort() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void checkPasswordOnlyLetters() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void checkPasswordOnlyNumbers() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void checkPasswordOnlySpecialCharacters() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void checkPasswordOnlyCapitals() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void checkPasswordNoNumbers() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void checkPasswordNoSpecialCharacters() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    void checkPasswordNoCapitals() {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+
+
+}
